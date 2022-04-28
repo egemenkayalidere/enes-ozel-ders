4c6f703c0143baf2d5b78bee2fc80609ee4f0723
Egemen Kayalidere <egemen.kayalidere@ardictech.com>
2022-04-28 22:26:50 +0300

    lesson

---
+++ index.js
@@ -0,0 +1,31 @@
+// console.log("egemen");
+// var element = document.getElementById("div1");
+// console.log(element);
+// element.style.width = "500px";
+// // document.getElementById("div2").style.backgroundColor = "blue";
+// // document.getElementById("div1").style.width = "500px";
+// alert(12321312312);
+// var result = confirm("Butona bas");
+// console.log("result1: ", result);
+// console.log("result2: ", result);
+// console.log(result);
+// console.log(result);
+
+// Veri Tipleri ----
+
+// var number = 1;
+// var string = "Enes";
+// var boolean = true;
+// var float = 0.5;
+
+// let number = 1;
+// number = 2;
+// console.log("number", number);
+
+// try {
+//   const pi = 3.14;
+//   pi = 5;
+//   console.log("number", number);
+// } catch (error) {
+//   console.log("error", error);
+// }
